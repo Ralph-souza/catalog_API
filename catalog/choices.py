e55e1e74926172abc99f9d4e01045b3b741b4cad
Ralph-souza <ralph_souza@hotmail.com>
2024-09-17 11:28:08 -0300

    Add application first models

---
+++ catalog/choices.py
@@ -0,0 +1,27 @@
+PLATFORM_CHOICES = (
+    ("playstation", "Playstation"), 
+    ("xbox", "XBox"), 
+    ("nintendo_switch", "Nintendo Switch"),
+    ("pc", "PC"),
+)
+
+CATEGORY_CHOICES = (
+    ("movies", "Movies"),
+    ("books", "Books"),
+    ("games", "Games"),
+    ("action_figures", "Action Figures"),
+)
+
+MEDIA_TYPE_CHOICES = (
+    ("dvd", "DVD"),
+    ("bluray", "Bluray"),
+    ("digital", "Digital"),
+    ("book", "Book"),
+    ("comics", "Comics"),
+    ("kindle", "Kindle"),
+)
+
+STATUS_CHOICES = (
+    ("available", "Available"),
+    ("loaned", "Loaned"),
+)
